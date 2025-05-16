63d4d14260fe807ba6c5e61cb90ab3ba634583da
Alexey Zotov <alexey1.zotov@yandex.ru>
2025-05-16 12:03:10 +0300

    Updated currency calculator with new studied maps slices pointers

--- calculator/main.go
+++ calculator/main.go
@@ -2,120 +2,93 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
-const (
-	USDEUR = 0.88
-	USDRUB = 81.49
-	EURRUB = 92.84
-)
-const (
-	EURUSD = 1 / USDEUR
-	RUBUSD = 1 / USDRUB
-	RUBEUR = 1 / EURRUB
-)
-
-func userMenu() {
-	initCurrency, targetCurrency := getCurrencies()
-	var amount float64
-	var err error
-
-	fmt.Println("Enter the amount of money to convert:")
-	for {
-		amount, err = getAmount()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		break
-	}
-	fmt.Printf("Converting: %.2f%s is %.2f%s\n",
-		amount, initCurrency, calculate(initCurrency, targetCurrency, amount), targetCurrency)
+var fxMap = map[string]float64{
+	"USDEUR": 0.88,
+	"USDRUB": 81.49,
+	"EURRUB": 92.84,
+	"EURUSD": 1 / 0.88,
+	"RUBUSD": 1 / 81.49,
+	"RUBEUR": 1 / 92.84,
 }
 
-func getCurrencies() (string, string) {
-	var initCurrency, targetCurrency string
+var currencies = []string{"USD", "EUR", "RUB"}
+
+func userMenu() {
+	requestedOperation := getOperation()
+	var amount float64
+
+	fmt.Print("Enter the amount of money to convert: ")
+	amount = getAmount()
+
+	fmt.Printf("Converting: %.2f%s is %.2f%s\n",
+		amount, requestedOperation[0:3], calculate(requestedOperation, amount),
+		requestedOperation[3:6])
+}
+
+func getOperation() string {
+	var operation, userInput string
 	var err error
-	fmt.Println("Enter your currency (accepted currencies: USD, EUR, RUB)")
-	for {
-		initCurrency, err = scanUserInput()
-		if err != nil {
-			fmt.Println(err)
-			continue
+
+	for i := 0; i < 2; i++ {
+		fmt.Printf("Enter your %d currency (accepted currencies: %v): ", i+1, currencies)
+		for {
+			userInput, err = scanUserInput()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if !checkCurrency(userInput, &currencies) {
+				continue
+			}
+			operation += userInput
+			break
 		}
-		if !checkCurrency(initCurrency) {
-			continue
-		}
-		break
 	}
-	fmt.Println("Enter targeted currency (accepted currencies: USD, EUR, RUB)")
-	for {
-		targetCurrency, err = scanUserInput()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if !checkCurrency(targetCurrency) {
-			continue
-		}
-		if targetCurrency == initCurrency {
-			fmt.Println("Currencies matching. Nothing to convert, please try again.")
-			continue
-		}
-		break
-	}
-	return initCurrency, targetCurrency
+	return operation
 }
 
 func scanUserInput() (string, error) {
 	var userInput string
-	_, err := fmt.Scan(&userInput)
+	_, err := fmt.Scanln(&userInput)
 	if err != nil {
 		return "", err
 	}
 	return userInput, nil
 }
 
-func checkCurrency(currency string) bool {
-	if currency != "USD" && currency != "EUR" && currency != "RUB" {
-		fmt.Println("Currency is not supported")
+func checkCurrency(currency string, currenciesList *[]string) bool {
+	var index int
+	if index = slices.Index(*currenciesList, currency); index == -1 {
+		fmt.Print("Currency is not supported!\nPlease try again: ")
 		return false
 	}
+	slices.Delete(currencies, index, index+1)
 	return true
 }
 
-func getAmount() (float64, error) {
+func getAmount() float64 {
 	var amount float64
-	_, err := fmt.Scan(&amount)
-	if err != nil {
-		return 0, err
+	for {
+		_, err := fmt.Scan(&amount)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		break
 	}
-	return amount, nil
+	return amount
 }
 
-func getFX(initCurrency, targetCurrency string) float64 {
-	fx := initCurrency + targetCurrency
-
-	switch fx {
-	case "USDEUR":
-		return USDEUR
-	case "USDRUB":
-		return USDRUB
-	case "EURRUB":
-		return EURRUB
-	case "RUBUSD":
-		return RUBUSD
-	case "RUBEUR":
-		return RUBEUR
-	case "EURUSD":
-		return EURUSD
-	default:
-		return 0
-	}
+func getFX(operation string, fx *map[string]float64) float64 {
+	fxValue, _ := (*fx)[operation]
+	return fxValue
 }
 
-func calculate(initCurrency, targetCurrency string, amount float64) float64 {
-	return getFX(initCurrency, targetCurrency) * amount
+func calculate(operation string, amount float64) float64 {
+	return getFX(operation, &fxMap) * amount
 }
 
 func main() {
