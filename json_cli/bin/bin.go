d9daae9941bc5792aebc36987e0af748706e6023
Alexey Zotov <alexey1.zotov@yandex.ru>
2025-05-18 10:46:03 +0300

    bins package added to json_cli

---
+++ json_cli/bin/bin.go
@@ -0,0 +1,34 @@
+package bins
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Bin struct {
+	Id        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+}
+
+func NewBin(isPrivate bool, binName string) Bin {
+	return Bin{
+		Id:        uuid.NewString(),
+		Private:   isPrivate,
+		CreatedAt: time.Now(),
+		Name:      binName,
+	}
+}
+
+type BinList struct {
+	Bins []Bin `json:"bins"`
+}
+
+func NewBinList() BinList {
+	return BinList{}
+}
+
+func (b *BinList) Add(newBin *Bin) {
+	b.Bins = append(b.Bins, *newBin)
+}
