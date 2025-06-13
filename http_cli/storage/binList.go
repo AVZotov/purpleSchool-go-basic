69feb90dfb32547fd5d4626d9f3227f7de6497ea
Alexey Zotov <alexey1.zotov@yandex.ru>
2025-06-13 18:44:20 +0300

    binList.go added delete method to remove records from local storage

--- http_cli/storage/binList.go
+++ http_cli/storage/binList.go
@@ -17,66 +17,42 @@ type BinListWithDb struct {
 	Db      LocalDb
 }
 
-func NewBinList() (*BinListWithDb, error) {
+func NewBinList() *BinListWithDb {
 	db := NewLocalDb()
-	bl := &BinListWithDb{
+	return &BinListWithDb{
 		BinList: BinList{
 			Data: []Bin{},
 		},
 		Db: *db,
 	}
-	err := bl.syncWithStorage()
-	if err != nil {
-		return nil, err
-	}
-	return bl, nil
 }
 
+// Create new record from metadata received and add this data
+// to local storage. It loads data from local storage. Converts metadata to Bin
+// add Bin to BinList and saves data back to local storage
 func (bl *BinListWithDb) Create(metadata []byte) error {
-	dataBin, err := toBin(metadata)
+	err := bl.Read()
 	if err != nil {
 		return err
 	}
-	bl.add(dataBin)
-	bytes, err := toBytes(&bl.BinList)
+	bin, err := toBin(metadata)
 	if err != nil {
 		return err
 	}
-	err = bl.Db.Write(bytes)
+
+	bl.add(bin)
+
+	err = bl.saveToLocalStorage()
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (bl *BinListWithDb) Read() ([]byte, error) {
-	data, err := bl.Db.Read()
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-// func (bl *BinListWithDb) Update(d Bin) error {
-//return error
-//}
-
-func (bl *BinListWithDb) Delete(d Bin) error {
-	return nil
-	// TODO remove local bin from list by ID and re save local file
-}
-
-func (bl *BinList) deleteById(id string) bool {
-	for i, bin := range bl.Data {
-		if bin.ID == id {
-			bl.Data = slices.Delete(bl.Data, i, i+1)
-			return true
-		}
-	}
-	return false
-}
-
-func (bl *BinListWithDb) syncWithStorage() error {
+// Read all data from local storage and add it to BinList
+// A successful call returns err == nil, not err == os.PathError
+// Because absence of local storage means first start of program or no data stored yet
+func (bl *BinListWithDb) Read() error {
 	bytes, err := bl.Db.Read()
 	pathError := &os.PathError{}
 	if err != nil {
@@ -95,8 +71,49 @@ func (bl *BinListWithDb) syncWithStorage() error {
 	return nil
 }
 
+// Delete record from local storage by ID. It loads local storage and convert it to BinList
+// metadata received converted to Bin, then BinList is searched by ID to remove data from local
+// storage. If record successfully removed, local storage resaved with updated data
+func (bl *BinListWithDb) Delete(id string) error {
+	err := bl.Read()
+	if err != nil {
+		return err
+	}
+	if !bl.BinList.deleteById(id) {
+		return fmt.Errorf("record with id: %s not found in local storage", id)
+	}
+
+	err = bl.saveToLocalStorage()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (bl *BinList) deleteById(id string) bool {
+	for i, bin := range bl.Data {
+		if bin.ID == id {
+			bl.Data = slices.Delete(bl.Data, i, i+1)
+			return true
+		}
+	}
+	return false
+}
+
 func (bl *BinListWithDb) add(bin *Bin) {
 	bl.BinList.Data = append(bl.BinList.Data, *bin)
+}
+
+func (bl *BinListWithDb) saveToLocalStorage() error {
+	bytes, err := toBytes(&bl.BinList)
+	if err != nil {
+		return err
+	}
+	err = bl.Db.Write(bytes)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func toBytes(binList *BinList) ([]byte, error) {
